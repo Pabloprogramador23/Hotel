b991856708ec7a752c0b50c9db27c1a01d0fbedb
Pablo Magalhães <Pablomagalhes@gmail.com>
2025-05-04 12:50:50 -0300

    prooddd

--- hotel_hms/settings.py
+++ hotel_hms/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'hotel_hms.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME', 'hotel_db'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
